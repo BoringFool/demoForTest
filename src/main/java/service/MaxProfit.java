e0606836273f07102394eee82862314fd520049a
borgni <1318593330@qq.com>
2023-10-05 16:24:56 +0800

    中等-动态规划-309：这样好理解一点

--- src/main/java/service/MaxProfit.java
+++ src/main/java/service/MaxProfit.java
@@ -104,18 +104,20 @@ public class MaxProfit {
     /**
      * 卖出后会有一天冷静期
      * 每天有三种状态：
-     *  买入/持有：昨天（i-1）已经买入过了的累计收益，或者前一天（i-2）卖出减去当天买
-     *       入支出的累计收益，取最大值。
-     *  卖出：昨天的买入最大累计收益加上当天卖出所得。对比昨天的卖出收益
-     *  什么都不做：和昨天一样。买入卖出都和昨天对比过了
+     * 买入/持有：昨天（i-1）已经买入过了的累计收益，或者前一天（i-2）卖出减去当天买
+     * 入支出的累计收益，取最大值。
+     * 卖出：昨天的买入最大累计收益加上当天卖出所得。对比昨天的卖出收益
+     * 什么都不做：和昨天一样。买入卖出都和昨天对比过了
+     *
      * @param prices
      * @return
      */
     public int maxProfit_V(int[] prices) {
         int[][] dp = new int[prices.length][2];
         dp[0][0] = -prices[0];//第一天买入，收益为负
-        dp[1][0] = Math.max(dp[0][0], -prices[1]) ;//记录两天内买入的最小花费
-        dp[1][1] = Math.max(0, dp[0][0] + prices[1]);//卖出则第一天必须买入，计算所得累计收益。
+        dp[0][1] = 0;
+        dp[1][0] = Math.max(dp[0][0], dp[0][1] - prices[1]);//记录两天内买入的最小花费
+        dp[1][1] = Math.max(dp[0][1], dp[0][0] + prices[1]);//卖出则第一天必须买入，计算所得累计收益。
 
         for (int i = 2; i < prices.length; i++) {
             dp[i][0] = Math.max(dp[i - 2][1] - prices[i], dp[i - 1][0]);
@@ -128,7 +130,7 @@ public class MaxProfit {
 
     public static void main(String[] args) {
 //        int[] a = new int[]{3, 3, 5, 0, 0, 3, 1, 4}; // 6
-        int[] a = new int[]{1,2,3,0,2}; // 4
+        int[] a = new int[]{1, 2, 3, 0, 2}; // 4
 //        int[] a = new int[]{7,6,4,3,1}; // 0
 //        int[] a = new int[]{1}; //0
 //        int[] a = new int[]{1,2,4,2,5,7,2,4,9,0}; //13
