e02fd8c6e08a4ab1e4136e9fb257a11a451bfc27
borgni <1318593330@qq.com>
2023-10-14 11:24:00 +0800

    完成题目要求需要位运算，想到了位运算，但看了题解。

---
+++ src/main/java/service/SingleNumber.java
@@ -0,0 +1,11 @@
+package service;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
+}
