1b946f039db2434a2eecdaa1dac6933ca1c163e8
borgni <1318593330@qq.com>
2023-10-15 10:25:09 +0800

    每次取出所有数字和二进制中的一位，然后模3，出现一次就是1，出现三次，则为0.

--- src/main/java/service/SingleNumber.java
+++ src/main/java/service/SingleNumber.java
@@ -8,4 +8,23 @@ public class SingleNumber {
         }
         return single;
     }
+
+    public int singleNumber_I(int[] nums) {
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {//取出每一位
+            int cnt = 0;
+            for (int num : nums) {
+                cnt += (num >> i) & 1;//统计当前位，所有的和
+            }
+
+            //0就不需要复位，1需要复位。加不加这个判断执行时间差不多。
+            if (cnt % 3 != 0) {
+                ans |= (1 << i);
+            }
+
+//            ans |= (cnt % 3) << i;
+        }
+
+        return ans;
+    }
 }
